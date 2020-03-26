66168f67b5c7e96e10622c6a971510d377a5d47f
thiagoverardo <38051267+thiagoverardo@users.noreply.github.com>
2020-03-26 12:02:43 -0300

    Rubrica C

---
+++ Labs/TC-RTT-IRQ/TC-RTC-IRQ/src/main.c
@@ -0,0 +1,355 @@
+#include "asf.h"
+/************************************************************************/
+/* DEFINES                                                              */
+/************************************************************************/
+
+/**
+ *  Informacoes para o RTC
+ *  poderia ser extraida do __DATE__ e 2
+ *  ou ser atualizado pelo PC.
+ */
+
+typedef struct
+{
+	uint32_t year;
+	uint32_t month;
+	uint32_t day;
+	uint32_t week;
+	uint32_t hour;
+	uint32_t minute;
+	uint32_t seccond;
+} calendar;
+
+/**
+* LEDs (EXT2!!!!!!!!!!!!!!!!)
+*/
+
+// Configuracoes do LED 1
+#define LED1_PIO           PIOC
+#define LED1_PIO_ID        ID_PIOC
+#define LED1_PIO_IDX       19
+#define LED1_PIO_IDX_MASK  (1u << LED1_PIO_IDX)
+
+// Configuracoes do LED 2
+#define LED2_PIO           PIOD
+#define LED2_PIO_ID        ID_PIOD
+#define LED2_PIO_IDX       26
+#define LED2_PIO_IDX_MASK  (1 << LED2_PIO_IDX)
+
+// Configuracoes do LED 3
+#define LED3_PIO           PIOD
+#define LED3_PIO_ID        ID_PIOD
+#define LED3_PIO_IDX       11
+#define LED3_PIO_IDX_MASK  (1 << LED3_PIO_IDX)
+
+/**
+* Botão
+*/
+#define BUT_PIO_ID ID_PIOA
+#define BUT_PIO PIOA
+#define BUT_PIN 11
+#define BUT_PIN_MASK (1 << BUT_PIN)
+#define BUT_DEBOUNCING_VALUE 79
+
+/************************************************************************/
+/* VAR globais                                                          */
+/************************************************************************/
+volatile char flag_led1 = 0;
+volatile char flag_rtc = 0;
+volatile char flag_tc = 0;
+volatile Bool f_rtt_alarme = false;
+
+/************************************************************************/
+/* PROTOTYPES                                                           */
+/************************************************************************/
+
+void BUT_init(void);
+void LED_init(int estado);
+void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type);
+void pin_toggle(Pio *pio, uint32_t mask);
+void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
+void io_init(void);
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses);
+
+/************************************************************************/
+/* Handlers                                                             */
+/************************************************************************/
+
+/**
+*  Handle Interrupcao botao 1
+*/
+static void Button1_Handler(uint32_t id, uint32_t mask)
+{
+}
+
+/**
+*  Interrupt handler for TC1 interrupt.
+*/
+void TC1_Handler(void){
+	volatile uint32_t ul_dummy;
+
+	/****************************************************************
+	* Devemos indicar ao TC que a interrupção foi satisfeita.
+	******************************************************************/
+	ul_dummy = tc_get_status(TC0, 1);
+
+	/* Avoid compiler warning */
+	UNUSED(ul_dummy);
+
+	/** Muda o estado do LED */
+	flag_tc = 1;
+}
+
+
+void pisca_led1(int n, int t){
+	for (int i=0;i<n;i++){
+		pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
+		delay_ms(t);
+		pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+		delay_ms(t);
+	}
+}
+
+void RTT_Handler(void)
+{
+	uint32_t ul_status;
+
+	/* Get RTT status - ACK */
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {  }
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		pin_toggle(LED2_PIO, LED2_PIO_IDX_MASK);    // BLINK Led
+		f_rtt_alarme = true;                  // flag RTT alarme
+	}
+}
+
+/**
+* \brief Interrupt handler for the RTC. Refresh the display.
+*/
+void RTC_Handler(void){
+	uint32_t ul_status = rtc_get_status(RTC);
+
+	/* Sec IRQ */
+	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC){
+		rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+	}
+
+	/* Time or date alarm */
+	if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM){
+		rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+		pin_toggle(LED3_PIO, LED3_PIO_IDX_MASK);
+	}
+	flag_rtc = 1;
+
+	rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
+	rtc_clear_status(RTC, RTC_SCCR_CALCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
+}
+
+/************************************************************************/
+/* Funcoes                                                              */
+/************************************************************************/
+
+//Pisca LED
+void pisca_led3(int n, int t){
+	for (int i=0;i<n;i++){
+		pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
+		delay_ms(t);
+		pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+		delay_ms(t);
+	}
+}
+
+/**
+*  Toggle pin controlado pelo PIO (out)
+*/
+void pin_toggle(Pio *pio, uint32_t mask){
+	if (pio_get_output_data_status(pio, mask))
+		pio_clear(pio, mask);
+	else
+		pio_set(pio, mask);
+}
+
+void io_init(void){
+	/* led */
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pio_configure(LED2_PIO, PIO_OUTPUT_0, LED2_PIO_IDX_MASK, PIO_DEFAULT);
+}
+static float get_time_rtt(){
+	uint ul_previous_time = rtt_read_timer_value(RTT);
+}
+
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
+{
+	uint32_t ul_previous_time;
+
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
+	
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 0);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN);
+}
+
+/**
+* @Brief Inicializa o pino do BUT
+*/
+void BUT_init(void){
+	/* config. pino botao em modo de entrada */
+	pmc_enable_periph_clk(BUT_PIO_ID);
+	pio_set_input(BUT_PIO, BUT_PIN_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+
+	/* config. interrupcao em borda de descida no botao do kit */
+	/* indica funcao (but_Handler) a ser chamada quando houver uma interrupção */
+	pio_enable_interrupt(BUT_PIO, BUT_PIN_MASK);
+	pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIN_MASK, PIO_IT_FALL_EDGE, Button1_Handler);
+
+	/* habilita interrupçcão do PIO que controla o botao */
+	/* e configura sua prioridade                        */
+	NVIC_EnableIRQ(BUT_PIO_ID);
+	NVIC_SetPriority(BUT_PIO_ID, 1);
+};
+
+/**
+* @Brief Inicializa o pino do LED
+*/
+void LED_init(int estado){
+	
+	// Ativa PIOs necessários
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	
+	// Inicializa LEDs como saída
+	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, estado, 0, 0);
+	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, estado, 0, 0);
+	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, estado, 0, 0);
+
+};
+/**
+* Configura o TC para funcionar
+*/
+void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
+	uint32_t ul_div;
+	uint32_t ul_tcclks;
+	uint32_t ul_sysclk = sysclk_get_cpu_hz();
+
+	/* Configura o PMC */
+	/* O TimerCounter é meio confuso
+	o uC possui 3 TCs, cada TC possui 3 canais
+	TC0 : ID_TC0, ID_TC1, ID_TC2
+	TC1 : ID_TC3, ID_TC4, ID_TC5
+	TC2 : ID_TC6, ID_TC7, ID_TC8
+	*/
+	pmc_enable_periph_clk(ID_TC);
+
+	/** Configura o TC para operar em  4Mhz e interrupçcão no RC compare */
+	tc_find_mck_divisor(freq, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
+	tc_init(TC, TC_CHANNEL, ul_tcclks | TC_CMR_CPCTRG);
+	tc_write_rc(TC, TC_CHANNEL, (ul_sysclk / ul_div) / freq);
+
+	/* Configura e ativa interrupçcão no TC canal 0 */
+	/* Interrupção no C */
+	NVIC_EnableIRQ((IRQn_Type) ID_TC);
+	tc_enable_interrupt(TC, TC_CHANNEL, TC_IER_CPCS);
+
+	/* Inicializa o canal 0 do TC */
+	tc_start(TC, TC_CHANNEL);
+}
+
+/**
+* Configura o RTC para funcionar com interrupcao de alarme
+*/
+void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type){
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_RTC);
+
+	/* Default RTC configuration, 24-hour mode */
+	rtc_set_hour_mode(rtc, 0);
+
+	/* Configura data e hora manualmente */
+	rtc_set_date(rtc, t.year, t.month, t.day, t.week);
+	rtc_set_time(rtc, t.hour, t.minute, t.seccond);
+
+	/* Configure RTC interrupts */
+	NVIC_DisableIRQ(id_rtc);
+	NVIC_ClearPendingIRQ(id_rtc);
+	NVIC_SetPriority(id_rtc, 0);
+	NVIC_EnableIRQ(id_rtc);
+
+	/* Ativa interrupcao via alarme */
+	rtc_enable_interrupt(rtc, irq_type);
+}
+
+/************************************************************************/
+/* Main Code	                                                        */
+/************************************************************************/
+int main(void){
+	/* Initialize the SAM system */
+	sysclk_init();
+	delay_init();
+	io_init();
+	/* Disable the watchdog */
+	WDT->WDT_MR = WDT_MR_WDDIS;
+
+	/* Configura Leds */
+	LED_init(0);
+
+	/* Configura os botões */
+	BUT_init();
+	
+	// Inicializa RTT com IRQ no alarme.
+	f_rtt_alarme = true;
+
+	/** Configura RTC */
+	calendar rtc_initial = {2018, 3, 19, 12, 15, 45, 1};
+	RTC_init(RTC, ID_RTC, rtc_initial, RTC_IER_ALREN);
+	/** Configura timer TC0, canal 1 */
+	TC_init(TC0, ID_TC1, 1, 4);
+
+	/* configura alarme do RTC */
+	rtc_set_date_alarm(RTC, 1, rtc_initial.month, 1, rtc_initial.day);
+	rtc_set_time_alarm(RTC, 1, rtc_initial.hour, 1, rtc_initial.minute, 1, rtc_initial.seccond + 20);
+
+	while (1)	{
+		/* Entrar em modo sleep */
+		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+		
+		if(flag_tc){
+			pisca_led1(1,10);
+			flag_tc = 0;
+		}
+		
+		if(flag_rtc){
+			pisca_led3(5, 200);
+			flag_rtc = 0;
+		}
+		if (f_rtt_alarme){
+      
+		  /*
+		   * IRQ apos 4s -> 8*0.5
+		   */
+		  uint16_t pllPreScale = (int) (((float) 32768) / 4.0);
+		  uint32_t irqRTTvalue = 8;
+      
+		  // reinicia RTT para gerar um novo IRQ
+		  RTT_init(pllPreScale, irqRTTvalue);         
+      
+		  f_rtt_alarme = false;
+		}
+	}
+}
